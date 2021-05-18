97b32e6cac31c1140e711632a36b389a52a11c6e
Mario Rubio <mario@mrrb.eu>
2021-05-18 11:00:30 +0200

    Fix MQTT buffer size; Use esp_log.h lib

--- baliza/src/mqtt/mqtt.c
+++ baliza/src/mqtt/mqtt.c
@@ -1,20 +1,23 @@
+#include "esp_log.h"
+
 #include <stddef.h>
 #include <stdio.h>
-#include "esp_log.h"
 #include "mqtt_client.h"
 #include "esp_wifi.h"
 #include "esp_system.h"
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_netif.h"
-#include "protocol_examples_common.h"
+#include "connect.h"
 
 #include "mqtt.h"
 #include "config.h"
 
-static const char *TAG = "MQTT_EXAMPLE";
 static esp_mqtt_client_handle_t client_st;
 static QueueHandle_t* p_solicitudDatosQueue = NULL;
+
+static const char *TAG = "MQTT_DRIVER";
+
 
 void init_mqtt(QueueHandle_t *solicitudDatosQ){
     p_solicitudDatosQueue = solicitudDatosQ;
@@ -22,138 +25,106 @@ void init_mqtt(QueueHandle_t *solicitudDatosQ){
 
 void connect_WIFI(){
 
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "[APP] Startup..");
-    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
-    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
-#endif
-
-    esp_log_level_set("*", ESP_LOG_INFO);
-    esp_log_level_set("MQTT_CLIENT", ESP_LOG_VERBOSE);
-    esp_log_level_set("MQTT_EXAMPLE", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT_BASE", ESP_LOG_VERBOSE);
-    esp_log_level_set("esp-tls", ESP_LOG_VERBOSE);
-    esp_log_level_set("TRANSPORT", ESP_LOG_VERBOSE);
-    esp_log_level_set("OUTBOX", ESP_LOG_VERBOSE);
+    ESP_LOGD(TAG, "[init_mqtt] Startup..");
+    ESP_LOGD(TAG, "[init_mqtt] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGD(TAG, "[init_mqtt] IDF version: %s", esp_get_idf_version());
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    ESP_ERROR_CHECK(example_connect());
+    ESP_ERROR_CHECK(connect());
 
 }
 
 static void log_error_if_nonzero(const char *message, int error_code)
 {
     if (error_code != 0) {
-        ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+        ESP_LOGE(TAG, "[log_error_if_nonzero] Last error %s: 0x%x", message, error_code);
     }
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
 {
-  ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%d", base, event_id);
+  ESP_LOGD(TAG, "[mqtt_event_handler] Event dispatched from event loop base=%s, event_id=%d", base, event_id);
   esp_mqtt_event_handle_t event = event_data;
 
   switch ((esp_mqtt_event_id_t)event_id)
   {
   case MQTT_EVENT_CONNECTED:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_CONNECTED");
     esp_mqtt_client_subscribe(client_st, CONFIG_MQTT_TOPIC_SOLICITUD, 0);
     break;
+
   case MQTT_EVENT_DISCONNECTED:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_DISCONNECTED");
     break;
+
   case MQTT_EVENT_SUBSCRIBED:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
-    printf("sent subscribe successful");
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+    ESP_LOGD(TAG, "[mqtt_event_handler] Sent subscribe successful");
     break;
+
   case MQTT_EVENT_UNSUBSCRIBED:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
     break;
+
   case MQTT_EVENT_PUBLISHED:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
     break;
+
   case MQTT_EVENT_DATA:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_DATA");
-#endif
+    ESP_LOGD(TAG, "[mqtt_event_handler] MQTT_EVENT_DATA");
     if (p_solicitudDatosQueue != NULL)
     {
-#ifdef DEBUG_PRINT_ENABLE
-      printf("TOPIC=%.*s\n", event->topic_len, event->topic);
-      printf("DATA=%.*s\n", event->data_len, event->data);
-#endif /*DEBUG_PRINT_ENABLE*/
-      char to_compare[256];
+      char to_compare[512];
+  
+      ESP_LOGI(TAG, "[mqtt_event_handler] Recv MQTT - TOPIC=%.*s", event->topic_len, event->topic);
+      ESP_LOGI(TAG, "[mqtt_event_handler] Recv MQTT - DATA=%.*s", event->data_len, event->data);
+  
       sprintf(to_compare, "%.*s", event->topic_len, event->topic);
-      // memcpy(to_compare, event->topic, event->topic_len);
-      // to_compare[event->topic_len + 1] = '\0';
-      printf(">> %d -- %s -- %s\n", strcmp(to_compare, CONFIG_MQTT_TOPIC_SOLICITUD), to_compare, CONFIG_MQTT_TOPIC_SOLICITUD);
 
       if (strcmp(to_compare, CONFIG_MQTT_TOPIC_SOLICITUD) == 0)
       {
         sprintf(to_compare, "%.*s", event->data_len, event->data);
-        // memcpy(to_compare, event->data, event->data_len);
-        // to_compare[event->data_len + 1] = '\0';
 
         bool txSolicitudDatos = false;
 
-        if (strcmp(to_compare, "true") == 0)
+        if (strcmp(to_compare, CONFIG_MQTT_TOPIC_SOLICITUD_MSG) == 0)
         {
           txSolicitudDatos = true;
         }
-#ifdef DEBUG_PRINT_ENABLE
-        printf("Se envia la senal de solicitudDatos a la fsm_emergencia.\n");
-#endif /* DEBUG_PRINT_ENABLE */
+        ESP_LOGD(TAG, "[mqtt_event_handler] Se envia la senal de solicitudDatos a la fsm_emergencia.");
 
         // Send solicitudDatos MQTT data to fsm_emergencia.
         if (xQueueSend(*p_solicitudDatosQueue, (void *)&txSolicitudDatos, (TickType_t)0) != pdTRUE)
         {
           // Failed to post the message.
-#ifdef DEBUG_PRINT_ENABLE
-          printf("Error en enviar senal de solicitudDatos.\n");
-#endif /* DEBUG_PRINT_ENABLE */
+          ESP_LOGW(TAG, "[mqtt_event_handler] Error en enviar senal de solicitudDatos.");
         }
         else
         {
-#ifdef DEBUG_PRINT_ENABLE
-          printf("Enviado senal de solicitudDatos = true.\n");
-#endif /* DEBUG_PRINT_ENABLE */
+          ESP_LOGD(TAG, "[mqtt_event_handler] Enviado senal de solicitudDatos = true.");
         }
       }
     }
     else
     {
-#ifdef DEBUG_PRINT_ENABLE
-      printf("Error: queue of solicitudDatos is not correctly open.\n");
-#endif /* DEBUG_PRINT_ENABLE */
+      ESP_LOGE(TAG, "[mqtt_event_handler] Queue of solicitudDatos is not correctly open.");
     }
     break;
+
   case MQTT_EVENT_ERROR: 
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
-#endif
+    ESP_LOGI(TAG, "[mqtt_event_handler] MQTT_EVENT_ERROR");
     if (event->error_handle->error_type != MQTT_ERROR_TYPE_NONE)
     {
       log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
       log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
     }
     break;
+
   default:
-#ifdef DEBUG_PRINT_ENABLE
-    ESP_LOGI(TAG, "Other event id:%d", event->event_id);
-#endif
+    ESP_LOGI(TAG, "[mqtt_event_handler] Other event id: %d", event->event_id);
     break;
   }
 }
